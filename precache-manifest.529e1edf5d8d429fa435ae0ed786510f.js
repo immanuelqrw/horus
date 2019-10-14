4867387a624d704b8bf6937d330c8e457434ede5
Immanuel Washington <code.immanuelqrw@gmail.com>
2019-10-14 00:52:27 -0400

    Updates

---
+++ precache-manifest.529e1edf5d8d429fa435ae0ed786510f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9be725c6f58471f70a23c3876c741ca",
+    "url": "horus.immanuelqrw.dev/index.html"
+  },
+  {
+    "revision": "a0a03deac3cba38edef4",
+    "url": "horus.immanuelqrw.dev/static/css/main.5155848c.chunk.css"
+  },
+  {
+    "revision": "d6a4ef2e94958062bc14",
+    "url": "horus.immanuelqrw.dev/static/js/2.76d89fc1.chunk.js"
+  },
+  {
+    "revision": "a0a03deac3cba38edef4",
+    "url": "horus.immanuelqrw.dev/static/js/main.5a9fa2bd.chunk.js"
+  },
+  {
+    "revision": "36b5ae34c463a48b460b",
+    "url": "horus.immanuelqrw.dev/static/js/runtime-main.9c4ff1ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "horus.immanuelqrw.dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
